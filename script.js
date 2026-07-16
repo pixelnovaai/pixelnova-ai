32fd708b6211fb80e82c14f802625b00cd6e04b3
pixelnovaai <asali20042005@gmail.com>
2026-07-16 16:54:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const API_KEY =5b7b8c5dca59aff57c7d518ee174ecdd103e77e666986275e60dc01d8c370d92b4719bfa707b7fb7eef3f3bef08dcc4c
 const fileInput = document.getElementById("fileInput");
 const previewImage = document.getElementById("previewImage");
 
@@ -32,4 +33,15 @@ document.getElementById("downloadBtn").addEventListener("click", function () {
 
     }
 
+});
+document.getElementById("editBtn").addEventListener("click", async () => {
+
+    const file = document.getElementById("fileInput").files[0];
+
+    if (!file) {
+        alert("Please upload an image first.");
+        return;
+    }
+
+    alert("AI is processing your image...");
 });
