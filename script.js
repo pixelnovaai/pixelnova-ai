bfcfba57badce73cf99f35dd3021249c9d6b96da
pixelnovaai <asali20042005@gmail.com>
2026-07-16 15:23:25 +0500

    Add file input change event listener

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const fileInput = document.getElementById("fileInput");
 
+fileInput.addEventListener("change", function(){
+
+if(this.files.length>0){
+
+alert("Selected: " + this.files[0].name);
+
+}
+
+});
